413908001ae5bfff9b1d7156a9f2cfd60a0fe871
vardhan-nyros <vardhan_nyros@yahoo.com>
2013-03-08 10:40:55 +0530

    initial commit

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,9 @@
+class SessionController < ApplicationController
+def create
+  auth = request.env["omniauth.auth"]
+  user = User.where(:provider => auth['provider'],
+                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
+  session[:user_id] = user.id
+  redirect_to root_url, :notice => "Signed in!"
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookApp::Application.config.secret_token = '0aa98410f06de396492668d03ce105001746b604961e4651c2a88fa8b62d8ccdec88523a3d4a2dc41ad9e1bd7d107f7e5e01e1866c9f21cb6474b4cce6e97edf'
